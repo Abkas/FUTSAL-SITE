f6ab238e0f63c928ddc34e5ed9d5a007355beb83
Abhishek Magar <abhishekmgr321@gmail.com>
2025-07-06 23:26:51 +0545

    Update jwt-token.js

--- backend/src/utils/jwt-token.js
+++ backend/src/utils/jwt-token.js
@@ -9,6 +9,7 @@ export const generateAccessToken = (userId,res) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     })
 
