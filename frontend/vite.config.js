4a15cba3b9c90f8667e5c4b6c16d4f4107cdf241
Abhishek Magar <abhishekmgr321@gmail.com>
2025-07-06 22:55:02 +0545

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
     server: {
     proxy: {
-      '/api/v1': 'https://matchpoint-i7tj.onrender.com/', 
+      '/api/v1': 'http://localhost:8000'
     }
   }
 })
